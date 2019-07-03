fd65bfca82cefe85371db8573093a94a0dde713d
Backslash <lanceadrian13@gmail.com>
2019-07-02 21:06:10 -0400

    finished coding jquery

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # unit-4-game
+
+psuedocode
+
+a game with 4 crystals and random result
+
+every crystal needs to have a random number between 1-12
+
+A new random number should be generated ever single time we win or lost to those 4 crystals
+
+when clicking any crystal, it should add the previous result
+
+until it equals the total score
+
+if it is not equal, then we start over
+
+if it is equal, then we increment a win counter
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,49 +4,149 @@ $( document ).ready(function() {
 
 
 
-var wins = 0;
-var loses = 0;
-var crystalscore =0;
+var wins = 0
+var loses = 0
+var random;
+var previous = 0 
+
+
+var setupGame = function(){
+    $(".crystals").empty();
+
+    var images = [
+        "http://uploads.webflow.com/5a8cfc09aee91400019461b0/5a8cffecaee91400019463c4_Asset%202%402x.png",
+        "http://uploads.webflow.com/5a8cfc09aee91400019461b0/5a8cffec2d6ad40001720f22_Asset%205%402x.png",
+        "http://uploads.webflow.com/5a8cfc09aee91400019461b0/5a8d073baee9140001946903_Asset%206%402x.png",
+        "http://uploads.webflow.com/5a8cfc09aee91400019461b0/5a8cffecaee91400019463c5_Asset%201%402x.png"]
+    
+
+    random = Math.floor(Math.random()* 102+19)
+
+    $("#answer").html(random);
 
 
 
-var random = Math.floor(Math.random()* 102+19)
- console.log(random);
- $("#answer").html(random);
+
+    for(var i = 0; i<4; i++){
+    var crystalNums = Math.floor(Math.random()* 12) + 1
+    
+    
+
+    var crystal = $("<div>")
+       crystal.attr({
+           "class": 'crystal',
+           "crystalvalue":crystalNums
+       });
+       crystal.css({
+           "background-image":"url('"+ (images[i]) + "')",
+           "background-size":"cover"
+       })
+
+
+    $(".crystals").append(crystal);
+}
+$("#crystaltotal").html(previous);
+}
+
+setupGame();
+
+$(document).on("click", ".crystal" ,function(){
+    var num = parseInt($(this).attr('crystalvalue'));
+    previous += num;
+
+    $("#crystaltotal").html(previous);
+
+    console.log(previous)
+
+    if(previous === random){
+        wins++;
+        $("#win").html("Wins: " + wins)
+        previous = 0;
+        setupGame();
+    }
+    else if (previous > random){
+        loses++;
+        $("#lose").html("Loses: " + loses)
+        previous = 0;
+        setupGame();
+    }
 
 
 
-$(crystal1).on("click", function(){
-   var crystal1Num = Math.floor(Math.random()* 12) + 1
-    console.log(crystal1Num);
-    $("#crystalresult").html(crystal1Num);
+});
 
-})
 
-$(crystal2).on("click", function(){
-    var crystal2Num = Math.floor(Math.random()* 12) + 1
-     console.log(crystal2Num);
-     function(){
-         if (!==0)
-     $("#crystalresult").html(crystal2Num);
-     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var random = Math.floor(Math.random()* 102+19)
+//  console.log(random);
+//  $("#answer").html(random);
+
+
+
+// $(crystal1).on("click", function(){
+//    var crystal1Num = Math.floor(Math.random()* 12) + 1
+//     console.log(crystal1Num);
+//     $("#crystalresult").html(crystal1Num);
+
+// })
+
+// $(crystal2).on("click", function(){
+//     var crystal2Num = Math.floor(Math.random()* 12) + 1
+//      console.log(crystal2Num);
+//      $("#crystalresult").html(crystal2Num);
+     
 
  
-})
+// })
 
-$(crystal3).on("click", function(){
-   var crystal3Num = Math.floor(Math.random()* 12) + 1
-    console.log(crystal3Num);
+// $(crystal3).on("click", function(){
+//    var crystal3Num = Math.floor(Math.random()* 12) + 1
+//     console.log(crystal3Num);
 
-    $("#crystalresult").html(crystal3Num);
+//     $("#crystalresult").html(crystal3Num);
 
-})
+// })
 
-$(crystal4).on("click", function(){
-    var crystal4Num = Math.floor(Math.random()* 12) + 1
-     console.log(crystal4Num);
-     $("#crystalresult").html(crystal4Num);
- })
+// $(crystal4).on("click", function(){
+//     var crystal4Num = Math.floor(Math.random()* 12) + 1
+//      console.log(crystal4Num);
+//      $("#crystalresult").html(crystal4Num);
+//  })
 
 
 
